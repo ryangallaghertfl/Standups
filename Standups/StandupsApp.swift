ff261b796028f6f56995d64b792f2ffa2d1981c0
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-06 17:01:25 +0000

    Initial Commit

---
+++ Standups/StandupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandupsApp.swift
+//  Standups
+//
+//  Created by Ryan Gallagher on 06/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct StandupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
