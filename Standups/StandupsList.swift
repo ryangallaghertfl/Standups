07d2e52d5b52c99071cf995803a5d530d9f1a86d
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-07 15:16:56 +0000

    added cardview from scrumdinger app

--- Standups/StandupsList.swift
+++ Standups/StandupsList.swift
@@ -40,18 +40,61 @@ struct StandupsListView: View {
     var body: some View {
         //only observing standups from the store
         WithViewStore(self.store, observe: \.standups) { viewStore in
-              List {
+            List {
                 ForEach(viewStore.state) { standup in
                 }
-              }
-        .navigationTitle("Daily Standups")
-        .toolbar {
-            ToolbarItem {
-                Button("Add"){}
+            }
+            .navigationTitle("Daily Standups")
+            .toolbar {
+                ToolbarItem {
+                    Button("Add"){}
+                }
             }
         }
     }
 }
+    
+    struct CardView: View {
+        let standup: Standup
+        
+        var body: some View {
+            VStack(alignment: .leading) {
+                Text(self.standup.title)
+                    .font(.headline)
+                Spacer()
+                HStack {
+                    Label(
+                        "\(self.standup.attendees.count)",
+                        systemImage: "person.3"
+                    )
+                    Spacer()
+                    Label(
+                        self.standup.duration.formatted(.units()),
+                        systemImage: "clock"
+                    )
+                    .labelStyle(.trailingIcon)
+                }
+                .font(.caption)
+            }
+            .padding()
+            .foregroundColor(self.standup.theme.accentColor)
+        }
+    }
+    
+    struct TrailingIconLabelStyle: LabelStyle {
+        func makeBody(
+            configuration: Configuration
+        ) -> some View {
+            HStack {
+                configuration.title
+                configuration.icon
+            }
+        }
+    }
+    
+    extension LabelStyle where Self == TrailingIconLabelStyle {
+        static var trailingIcon: Self { Self() }
+    }
 
 #Preview {
     MainActor.assumeIsolated {
