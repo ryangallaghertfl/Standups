39a7dc5a5a31bf3b6e188d29cb15d0705ddac790
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-07 15:03:25 +0000

    StandupsListFeature switches on addbuttontapped and will append standup struct to identified array

--- Standups/StandupsList.swift
+++ Standups/StandupsList.swift
@@ -10,19 +10,28 @@ import ComposableArchitecture
 
 struct StandupsListFeature: Reducer {
   struct State {
-    
+      //array uses unique id rather than array index
+    var standups: IdentifiedArrayOf<Standup> = []
   }
   enum Action {
-  
+    case addButtonTapped
   }
   var body: some ReducerOf<Self> {
     Reduce { state, action in
       switch action {
-
+      case .addButtonTapped:
+        state.standups.append(
+          Standup(
+            id: UUID(),
+            theme: .allCases.randomElement()!
+          )
+        )
+        return .none
       }
     }
   }
 }
+
 
 
 struct StandupsListView: View {
