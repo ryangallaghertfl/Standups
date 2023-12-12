536cf81883db1319883866f8ce426c6c2e71f29a
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-12-12 08:57:18 +0000

    standupformfeature reduce case deleteattendees, added guard clause and setting focus

--- Standups/StandUpForm.swift
+++ Standups/StandUpForm.swift
@@ -9,43 +9,53 @@ import ComposableArchitecture
 import SwiftUI
 
 struct StandupFormFeature: Reducer {
-  struct State {
-      @BindingState var focus: Field?
-      @BindingState var standup: Standup
-      
-      enum Field: Hashable {
-          case attendee(Attendee.ID)
-          case title
+  struct State: Equatable {
+    @BindingState var focus: Field?
+    @BindingState var standup: Standup
+
+    enum Field: Hashable {
+      case attendee(Attendee.ID)
+      case title
+    }
+
+    init(focus: Field? = .title, standup: Standup) {
+      self.focus = focus
+      self.standup = standup
+      if self.standup.attendees.isEmpty {
+        self.standup.attendees.append(Attendee(id: UUID()))
       }
-      
-      init(focus: Field? = .title, standup: Standup) {
-          self.focus = focus
-          self.standup = standup
-          if self.standup.attendees.isEmpty {
-              self.standup.attendees.append(Attendee(id: UUID()))
-          }
-      }
+    }
   }
-    enum Action: BindableAction {
-      case addAttendeeButtonTapped
-      case binding(BindingAction<State>)
-      case deleteAttendees(atOffsets: IndexSet)
+  enum Action: BindableAction {
+    case addAttendeeButtonTapped
+    case binding(BindingAction<State>)
+    case deleteAttendees(atOffsets: IndexSet)
   }
-    
+  @Dependency(\.uuid) var uuid
   var body: some ReducerOf<Self> {
-      BindingReducer()
-      Reduce { state, action in
-          switch action {
-          case .addAttendeeButtonTapped:
-              let id = UUID()
-              state.standup.attendees.append(Attendee(id: id))
-              state.focus = .attendee(id)
-              return .none
-          case .binding(_):
-              return .none
-          case .deleteAttendees(atOffsets: let atOffsets):
-              <#code#>
-          }
+    BindingReducer()
+    Reduce { state, action in
+      switch action {
+      case .addAttendeeButtonTapped:
+        let id = self.uuid()
+        state.standup.attendees.append(Attendee(id: id))
+        state.focus = .attendee(id)
+        return .none
+
+      case .binding(_):
+        return .none
+
+      case let .deleteAttendees(atOffsets: indices):
+        state.standup.attendees.remove(atOffsets: indices)
+        if state.standup.attendees.isEmpty {
+          state.standup.attendees.append(Attendee(id: self.uuid()))
+        }
+        guard let firstIndex = indices.first
+        else { return .none }
+        let index = min(firstIndex, state.standup.attendees.count - 1)
+        state.focus = .attendee(state.standup.attendees[index].id)
+        return .none
+      }
     }
   }
 }
